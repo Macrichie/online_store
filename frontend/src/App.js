7ce2f78b401708f6d10ab82fd0b734488748b8bb
Macrichie <makanjuolakunle@gmail.com>
2022-01-19 12:00:24 +0000

    initial commit create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Online Store</a>
+      </header>
+      <main>List Items</main>
+    </div>
+  );
+}
+
+export default App;
